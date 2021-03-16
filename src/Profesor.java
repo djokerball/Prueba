f7bde4d02499b528c6f7724ab1a2eedcf80d022a
Pepe <s.frost1812@gmail.com>
2021-03-16 19:15:16 +0100

    Implementado constructor

--- src/Profesor.java
+++ src/Profesor.java
@@ -4,4 +4,13 @@ public class Profesor {
 	private String nombre;
 	private String dni;
 	private int telefono;
+	
+	public Profesor(String nombre, String dni, int telefono) {
+		super();
+		this.nombre = nombre;
+		this.dni = dni;
+		this.telefono = telefono;
+	}
+	
+	
 }
